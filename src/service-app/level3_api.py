ae27c1302076ced774bf75bc901171452a8a0b52
c-ashwini <ashwinireddy497@gmail.com>
2024-09-17 13:41:09 -0700

    Change level3 encryption to vigenère

--- src/service-app/level3_api.py
+++ src/service-app/level3_api.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from rest_api import app
 @app.route('/api/level3/encryption-method', methods=['GET'])
 def encryption_method():
-    return jsonify({"method": "Caesar"})
+    return jsonify({"method": "Vigenère"})
 
 
 @app.route('/api/level3/validate', methods=['POST'])
