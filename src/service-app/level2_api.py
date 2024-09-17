fe828eef36015248f493f2cdd3d4819fdeb7d9c6
Kishore Pendyala <kishore.pendyala@xandr.com>
2024-09-17 10:34:27 -0400

    updated mock text for level2

--- src/service-app/level2_api.py
+++ src/service-app/level2_api.py
@@ -8,10 +8,10 @@ def sos_messages():
     data = {
         "message": "Requesting help. Come and get me!",
         "planets": [
-            {"name": "Mercury", "coordinates": {"x": 0.39, "y": 0.0}, "description": "Closest planet to the Sun"},
-            {"name": "Venus", "coordinates": {"x": 0.72, "y": 0.0}, "description": "Second planet from the Sun"},
-            {"name": "Earth", "coordinates": {"x": 1.0, "y": 0.0}, "description": "Our home planet"},
-            {"name": "Mars", "coordinates": {"x": 1.52, "y": 0.0}, "description": "Known as the Red Planet"}
+            {"name": "Mercury", "coordinates": {"latitude": 0.39, "longitude": 0.0}, "description": "Closest planet to the Sun"},
+            {"name": "Venus", "coordinates": {"latitude": 0.72, "longitude": 0.0}, "description": "Second planet from the Sun"},
+            {"name": "Earth", "coordinates": {"latitude": 1.0, "longitude": 0.0}, "description": "Our home planet"},
+            {"name": "Mars", "coordinates": {"latitude": 1.52, "longitude": 0.0}, "description": "Known as the Red Planet"}
         ]
     }
     return jsonify(data)
